7c01d837cd94abb31a9979c069bf2b12d8a2539d
Gilophe <149219442+Gilophe@users.noreply.github.com>
2025-06-15 14:13:58 +0300

    Update pickFamily.fg

--- pickFamily.py
+++ pickFamily.py
@@ -16,6 +16,10 @@ youtube = "https://youtube.com/@iewil"
 hosts = [
 "https://litepick.io/",
 "https://tronpick.io/",
+"https://dogepick.io/",
+"https://polpick.io/",
+"https://solpick.io/",
+"https://suipick.io/",
 "https://bnbpick.io/",
 "https://tonpick.game/"
 ]
